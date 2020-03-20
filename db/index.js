22e496b813a4baa7de11546c2f82a0dabb4801e3
jeremyrrose <jeremy.r.rose@gmail.com>
2020-03-20 19:50:21 -0400

    mongo

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/scrabblebags', { useUnifiedTopology: true, useNewUrlParser: true })
+    .connect(MONGODB_URI ? MONGODB_URI : 'mongodb://127.0.0.1:27017/scrabblebags', { useUnifiedTopology: true, useNewUrlParser: true })
     .then(() => {
         console.log('Successfully connected to MongoDB.');
       })
